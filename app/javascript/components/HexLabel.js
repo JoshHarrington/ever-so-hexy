06e9674485a4ad5a2859eddc7716343c52de08bf
Sam Lester <sam.lester123@gmail.com>
2021-06-03 18:59:29 +0100

    Tidied up styling and formatting

--- app/javascript/components/HexLabel.js
+++ app/javascript/components/HexLabel.js
@@ -1,14 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react'
 import classNames from "classnames"
+import moment from 'moment'
+import { countries } from 'country-data'
 
 const HexLabel = ({focusedHexInfo}) => {
 
-  console.log(focusedHexInfo)
+  function getFlagEmoji(countryCode) {
+
+    const codePoints = countryCode.toUpperCase().split('').map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+      
+  }
 
 	return (
     <>
       {focusedHexInfo ?
-        <div className="fixed bottom-0 text-blueGray-800 bg-white shadow rounded-16xl p-6">{focusedHexInfo.location}</div>
+        <ul className="fixed bottom-8 left-8 text-blueGray-800 bg-white shadow rounded-16xl py-4 px-6 font-bold">
+          <li className="inline-block">#{focusedHexInfo.order}</li>
+          <li className="inline-block pl-4">{moment(focusedHexInfo.created_at).fromNow()}</li>
+          {focusedHexInfo.country_code ?
+            <li className="inline-block pl-4">
+              {countries[focusedHexInfo.country_code].name}
+              <span className="pl-2 text-xl leading-none relative top-0.5">{getFlagEmoji(focusedHexInfo.country_code)}</span>
+            </li>
+            :
+            null
+          }
+        </ul>
         :
         <div>No Hex</div>
       }
