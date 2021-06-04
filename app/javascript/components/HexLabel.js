330c61a44d834a3ec1ffe55042da7a3eb80a795f
Sam Lester <sam.lester123@gmail.com>
2021-06-04 14:05:16 +0100

    Added bullet dividers

--- app/javascript/components/HexLabel.js
+++ app/javascript/components/HexLabel.js
@@ -17,13 +17,13 @@ const HexLabel = ({focusedHexInfo}) => {
 	return (
     <>
       {focusedHexInfo &&
-        <ul className="fixed bottom-8 left-8 text-blueGray-800 bg-white shadow rounded-16xl py-4 px-6 font-bold">
+        <ul className="bullet-seperator fixed bottom-8 left-8 text-blueGray-800 bg-white shadow rounded-16xl py-4 px-6 font-bold">
           {!focusedHexInfo.draft ?
             <>
               <li className="inline-block">#{focusedHexInfo.order}</li>
-              <li className="inline-block pl-4">{moment(focusedHexInfo.created_at).fromNow()}</li>
+              <li className="inline-block">{moment(focusedHexInfo.created_at).fromNow()}</li>
               {focusedHexInfo.country_code &&
-                <li className="inline-block pl-4">
+                <li className="inline-block">
                   {countries[focusedHexInfo.country_code].name}
                   <span className="pl-2 text-xl leading-none relative top-0.5">{getFlagEmoji(focusedHexInfo.country_code)}</span>
                 </li>
