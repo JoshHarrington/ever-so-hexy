fbeae5849209c9086ed127c934f46f3fc0dcdd9f
Sam Lester <sam.lester123@gmail.com>
2021-06-08 18:18:00 +0100

    Mobile styles for HexLabel

--- app/javascript/components/HexLabel.js
+++ app/javascript/components/HexLabel.js
@@ -14,13 +14,13 @@ const HexLabel = ({focusedHexInfo}) => {
 	return (
     <>
       {focusedHexInfo &&
-        <ul className="bullet-seperator text-blueGray-800 bg-white shadow rounded-16xl py-4 px-6 font-bold mx-auto">
+        <ul className="bullet-seperator fixed text-blueGray-800 bg-white shadow py-4 px-6 font-bold bottom-0 w-full text-center sm:bottom-8 sm:left-8 sm:w-auto sm:rounded-16xl">
           {!focusedHexInfo.draft ?
             <>
               <li className="inline-block">#{focusedHexInfo.order}</li>
               <li className="inline-block">{moment(focusedHexInfo.created_at).fromNow()}</li>
               {focusedHexInfo.country_code &&
-                <li className="inline-block">
+                <li className="country block sm:inline-block">
                   {countries[focusedHexInfo.country_code].name}
                   <span className="pl-2 text-xl leading-none relative top-0.5">{getFlagEmoji(focusedHexInfo.country_code)}</span>
                 </li>
