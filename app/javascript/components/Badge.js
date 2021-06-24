3c83c584e2baedcc2670d17daeab82fd00a11b6a
Sam Lester <sam.lester123@gmail.com>
2021-06-24 18:24:42 +0100

    Added mobile styles for info panel

--- app/javascript/components/Badge.js
+++ app/javascript/components/Badge.js
@@ -27,7 +27,7 @@ const TextBadge = ({href, onClick, className, hasWhiteBackground, children}) => 
 				className,
 				{"text-white shadow bg-teal-600 hover:bg-teal-700": !hasWhiteBackground},
 				{"text-teal-600 bg-white": hasWhiteBackground},
-				"border-0 border-solid py-2 px-4 rounded-full",
+				"border-0 border-solid py-3 px-4 rounded-full font-bold	",
 				"focus:outline-none",
 				"flex items-center pointer-events-auto",
 				"hover:ring hover:ring-teal-500-a25 focus:ring focus:ring-teal-500-a50"
