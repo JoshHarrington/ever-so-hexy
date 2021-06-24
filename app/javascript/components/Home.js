4d2ad29a5b475fe44fd4b95866beba8d7041aa21
Sam Lester <sam.lester123@gmail.com>
2021-06-24 18:57:21 +0100

    Added intro text

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -70,10 +70,14 @@ const Home = ({allHexes, lastHexOrderPosition}) => {
       {infoBlockShown ?
         <div className="fixed bottom-0 right-0 text-blueGray-800 w-full bg-white shadow sm:rounded-tl-16xl p-8 sm:max-w-xs">
           <h1 className="text-2xl font-black mb-2">Ever So Hexy</h1>
-          <p className="mb-5">An experimental collaborative art project by <a href="#na" className="font-bold">@Josh_Harrington</a> and <a href="#na" className="font-bold">@samlester</a></p>
+          <p className="mb-3">An experimental collaborative art project by <a href="#na" className="font-bold">@Josh_Harrington</a> and <a href="#na" className="font-bold">@samlester</a></p>
           { moreInfoShown ?
             <div>
-              <p>This is some more detail on the project</p>
+              <p className="mb-3">This project is designed to be a more pleasant way of spending that spare couple of minutes in your day. It's meant to be a palette-cleanser for your mind!</p>
+
+              <p className="mb-3">Create your own interesting shapes and patterns. Get inspired by other people creating fun hexagon designs. Find ways to link designs together in ways their creators didn't imagine.</p>
+
+              <p>This is a calm and welcoming place to design whatever hexagon you want, bear in mind we'll be removing any hexagons which don't fit that mindset.</p>
             </div>
               :
             <button
