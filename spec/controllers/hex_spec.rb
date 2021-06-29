d8f03ae02f495355ff04191455bd28baac326ebb
Josh Harrington <josh@joshharrington.co.uk>
2021-06-29 18:30:27 +0100

    Add status check to hexes contoller test Add local.rb to gitignore for local notes

--- spec/controllers/hex_spec.rb
+++ spec/controllers/hex_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe HexesController do
     subject { get :home }
 
     it "renders the index template" do
+      expect(response.status).to eq(200)
+
       expect(subject).to render_template(:home)
       expect(subject).to render_template("home")
       expect(subject).to render_template("hexes/home")
@@ -19,29 +21,12 @@ RSpec.describe HexesController do
     end
   end
 
-  describe "Check home view contents" do
-    render_views
-
-    let!(:hex) { create(:hex, draft: false) }
-
-    it "renders the index page" do
-      get :home
-
-      expect(response.status).to eq(200)
-
-      expect(response.body).to include("data-react-props=\"{&quot;allHexes&quot;:[{&quot;draft&quot;:false,&quot;order&quot;:0},{&quot;draft&quot;:false,&quot;order&quot;:0,&quot;trixel_colour_a1&quot;:&quot;#fff")
-
-      ## Need to get the server rendering setup before this will work
-      # expect(response.body).to match /<p>An experimental collaborative art project.*/im
-      expect(response.body).to match /<title>Ever So Hexy<\/title>/im
-    end
-
-  end
-
   describe "GET #new" do
     subject { get :new }
 
     it "renders the new template" do
+      expect(response.status).to eq(200)
+
       expect(subject).to render_template(:new)
       expect(subject).to render_template("new")
       expect(subject).to render_template("hexes/new")
@@ -55,18 +40,4 @@ RSpec.describe HexesController do
       expect(subject).to render_template("layouts/application")
     end
   end
-
-  # describe "Check new view contents" do
-  #   render_views
-
-
-  #   it "renders the index page" do
-  #     get :new
-
-  #     expect(response.status).to eq(200)
-
-  #     expect(response.body).to include("data-react-props=\"{&quot;allHexes&quot;:[{&quot;draft&quot;:true,&quot;order&quot;:1,&quot;trixel_colour_a1&quot;:&quot;#fff")
-  #     expect(response.body).to match /<title>Ever So Hexy | New Hexagon<\/title>/im
-  #   end
-  # end
 end
