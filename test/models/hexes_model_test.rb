c35f3bd8271226b14346c8a5ec13e634bfaa4f3b
Josh Harrington <josh@joshharrington.co.uk>
2021-07-04 00:19:48 +0100

    Rename clean_hex_array  - >> remove_non_public_hexes_data Add tests for non_public hex helper  - fix duplications found in non_public hex helper Extend Hex model test Update Hex model to allow order setting

--- test/models/hexes_model_test.rb
+++ test/models/hexes_model_test.rb
@@ -5,7 +5,7 @@ class ArticleTest < ActiveSupport::TestCase
     hex = Hex.new
 		assert hex.save
 
-		assert Hex.all.count == 1
+		assert Hex.count == 1
 
 		assert hex.draft == true
 		assert hex.trixel_colour_a1 == "#fff"
@@ -16,10 +16,46 @@ class ArticleTest < ActiveSupport::TestCase
 	test "order of hexes matches expected" do
 		hexes = Hex.create([{},{},{}])
 
-		assert Hex.all.count == 3
+		assert Hex.count == 3
 
 		assert hexes[0].order == 1
 		assert hexes[1].order == 2
 		assert hexes[2].order == 3
 	end
+
+	test "order of hexes matches defined" do
+		hexes = Hex.create([{order: 3},{order:1},{order:2}])
+
+		assert Hex.count == 3
+
+		assert hexes[0].order == 3
+		assert hexes[1].order == 1
+		assert hexes[2].order == 2
+	end
+
+	test "new hex gets correct order" do
+		hexes = Hex.create([{order: 3},{order:2}])
+
+		assert Hex.count == 2
+
+		new_hex = Hex.create()
+
+		assert Hex.count == 3
+		assert new_hex.order == 1
+
+	end
+
+	test "new hex with order matching an existing hex do not get created" do
+		hexes = Hex.create([{order: 3},{order:2}])
+
+		assert Hex.count == 2
+
+		new_hex = Hex.create(order: 3)
+
+		assert Hex.count == 2
+
+		assert new_hex.id == nil
+
+	end
+
 end
